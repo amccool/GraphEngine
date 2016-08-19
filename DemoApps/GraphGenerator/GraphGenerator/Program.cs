d61cfe49bd8977f672a65e32c35af321925bc398
Bin Shao <binshao@live.com>
2016-08-20 00:44:55 +0800

    Adds 5 demo csharp projects.

---
+++ DemoApps/GraphGenerator/GraphGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphGen gg = new GraphGen(1 << 20, 16, 100);
+            gg.CreatGraph();
+        }
+    }
+}
